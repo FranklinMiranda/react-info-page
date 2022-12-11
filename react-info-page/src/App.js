060c988864090f1800bb93a67052df37996e8299
FranklinMiranda <fdm61344@gmail.com>
2022-12-11 10:56:05 -0500

    CM

--- react-info-page/src/App.js
+++ react-info-page/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Info from './Info';
 import Table from './Table';
+import NavBar from "./Nav"
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <NavBar />
         <Info />
         <Table />
       </div>
