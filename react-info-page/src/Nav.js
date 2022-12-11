1f034164bce0d2f79217a293336cdd53660182bf
FranklinMiranda <fdm61344@gmail.com>
2022-12-11 10:54:51 -0500

    CM

---
+++ react-info-page/src/Nav.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Information</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+
