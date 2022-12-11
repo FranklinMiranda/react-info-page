e500a88b7efe74d6ca66f46ff9f8809339ca56ed
FranklinMiranda <fdm61344@gmail.com>
2022-12-11 11:41:57 -0500

    CM

--- react-info-page/src/App.js
+++ react-info-page/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import NavBar from './Nav';
 import Info from './Info';
 import Table from './Table';
-import NavBar from './Nav';
-import Form from './Form'
+import Form from './Form';
 
 class App extends React.Component {
   render() {
