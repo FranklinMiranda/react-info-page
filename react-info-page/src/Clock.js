e44b00d0465c5a9524eb64a9d60ac18329028ad4
FranklinMiranda <fdm61344@gmail.com>
2022-12-12 17:55:56 -0500

    Cm

--- react-info-page/src/Clock.js
+++ react-info-page/src/Clock.js
@@ -6,6 +6,22 @@ class Clock extends React.Component {
     this.state = { date: new Date() };
   }
 
+  // componentDidMount only runs once, when the component is inserted into the DOM so the setInterval method begins to run at the designated interval
+  componentDidMount() {
+    return this.timeID = setInterval(() => {return this.tick()}, 1000);
+  }
+
+  // componentWillUnmount only runs once if the Clock component is removed so the timer is stopped
+  componentWillUnmount(){
+    clearInterval(this.timeID)
+  }
+
+  tick() {
+    this.setState({
+        date: new Date()
+    });
+  }
+
   render() {
     return (
       <div>
