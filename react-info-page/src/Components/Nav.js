43741f46c08fbb9f3e0d5ca87011f7d2f6b12450
FranklinMiranda <fdm61344@gmail.com>
2022-12-13 12:08:20 -0500

    Cm

--- react-info-page/src/Components/Nav.js
+++ react-info-page/src/Components/Nav.js
@@ -3,13 +3,14 @@ import Image from '../Images/logo192.png';
 const NavBar = () => {
   return (
     <div>
-      <nav className='nav'>
-        <img className="Img" src={Image} alt="React"></img>
-        <ul>
-          <li>Home</li>
-          <li>Information</li>
-          <li>About</li>
-        </ul>
+      <nav className="Topnav">
+        <a href="#Home">
+          <img className="Img" src={Image} alt="React" />
+        </a>
+        <a href="#Information">Information</a>
+        <a href="#Contact">Contact</a>
+        <a href="#About">About</a>
+        <input type='text' placeholder="Search..." />
       </nav>
     </div>
   );
--- react-info-page/src/index.js
+++ react-info-page/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './CSS/index.css';
 import App from './Components/App';
+import './CSS/index.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
