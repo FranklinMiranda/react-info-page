e931e073da6655a43f6b407ad6bf55cee98f6d1f
FranklinMiranda <fdm61344@gmail.com>
2022-12-13 14:26:01 -0500

    Cm

--- react-info-page/src/Components/Clock.js
+++ react-info-page/src/Components/Clock.js
@@ -3,30 +3,45 @@ import React from 'react';
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { time: new Date(), timeOfDay: '' };
   }
 
   // componentDidMount only runs once, when the component is inserted into the DOM so the setInterval method begins to run at the designated interval
   // An ID is assigned to the setInterval Function that can be passed to the clearInterval function when setInterval is to stop executing
   componentDidMount() {
-    return this.timeID = setInterval(() => {return this.tick()}, 1000);
+    return (this.timeID = setInterval(() => {
+      return this.tick();
+    }, 1000));
   }
 
   // componentWillUnmount only runs once if the Clock component is removed so the timer is stopped
-  componentWillUnmount(){
-    clearInterval(this.timeID)
+  componentWillUnmount() {
+    clearInterval(this.timeID);
   }
 
   tick() {
     this.setState({
-        date: new Date()
+      time: new Date(),
     });
+
+    const hours = this.state.time.getHours();
+
+    if (hours < 12) {
+      this.setState({
+        timeOfDay: 'Morning',
+      });
+    } else if (hours >= 12 && hours < 16) {
+      this.setState({ timeOfDay: 'Afternoon' });
+    } else {
+      this.setState({ timeOfDay: 'Night' });
+    }
   }
 
   render() {
     return (
-      <div className='Clock'>
-        <h1>This is the current time: {this.state.date.toLocaleTimeString()}</h1>
+      <div className="Clock">
+        <h1>This is the current time: {this.state.time.toLocaleTimeString()}</h1>
+        <h1>Good {this.state.timeOfDay}!</h1>
       </div>
     );
   }
