1d3a260727ea7d158e40729d16e2fb7386c1d6d5
FranklinMiranda <fdm61344@gmail.com>
2022-12-14 12:12:01 -0500

    Cm

---
+++ react-info-page/src/Components/Info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import links from '../Data/InfoData';
+
+// This Info Component imports data from a JavaScript file and renders in HTML tags to the DOM
+class Info extends React.Component {
+  render(props) {
+    return;
+    <div>
+      <h1>Name : this.props.name</h1>
+      <h1>Link : this.props.link</h1>
+    </div>;
+  }
+}
+
+export default Info;
---
+++ react-info-page/src/Data/InfoData.js
@@ -0,0 +1,9 @@
+// This file contains a array of Wikipedia Links that will be exported to the Info Component
+
+const links = [
+  { name: 'Apple Inc.', hyperlink: 'https://en.wikipedia.org/wiki/Apple_Inc.' },
+  { name: 'Google', hyperlink: 'https://en.wikipedia.org/wiki/Google' },
+  { name: 'Oracle Corporation', hyperlink: 'https://en.wikipedia.org/wiki/Oracle_Corporation' },
+];
+
+export default links
