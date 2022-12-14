95315e6d9073f93459fba24a9170ce1c5d84dd4a
FranklinMiranda <fdm61344@gmail.com>
2022-12-14 13:46:57 -0500

    Cm

--- react-info-page/src/Components/Info.js
+++ react-info-page/src/Components/Info.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 // This Info Component imports data from a JavaScript file and renders in HTML tags to the DOM
+// Target set to blank opens the hyperlink in a new tab instead of the current tab
 class Info extends React.Component {
   render(props) {
     return (
       <div className="Info">
-        <h4><a href={this.props.hyperlink}>{this.props.name}</a></h4>
+        <h4><a href={this.props.hyperlink} target="_blank">{this.props.name}</a></h4>
       </div>
     );
   }
